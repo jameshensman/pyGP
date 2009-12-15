fixed the scaling of the samples from the posterior - I think the prior samples are still TODO

src/pyGP/regression/basic_regression.py
@@ -52,7 +52,7 @@ class GP:
 			assert parameter_priors.size==(self.kernel.nparams+1)
 			self.parameter_prior_widths = np.array(parameter_priors).flatten()
 		if prior_mean is None:
-			self.prior_mean = lambda X: pb.zeros(len(X))
+			self.prior_mean = lambda X: np.zeros(len(X))
 		else:
 			self.prior_mean = prior_mean
 			
@@ -214,8 +214,9 @@ class GP:
 	def sample(self,X):
 		if hasattr(self,'X'):
 			mean, variance = self.predict(X)
-			v = np.linalg.solve(self.L,self.kernel(X,self.X).T)
-			covariance = (self.kernel(X,X) - np.dot(v.T,v))
+			X2 = (X-self.xmean)/self.xstd # normalised version of points to sample
+			v = np.linalg.solve(self.L,self.kernel(X2,self.X).T)
+			covariance = (self.kernel(X2,X2) - np.dot(v.T,v))
 		else:
 			mean = self.prior_mean(X)
 			covariance = self.kernel(X,X)

src/testing/test_GP.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 import numpy as np
 import pylab as pb
@@ -22,15 +23,12 @@ def plot():
 	pb.plot(xx,yy,scaley=False)
 	pb.plot(xx,yy + 2*np.sqrt(cc),'k--',scaley=False)
 	pb.plot(xx,yy - 2*np.sqrt(cc),'k--',scaley=False)
+	# draw some samples
+	for i in range(4):
+		pb.plot(xx,myGP.sample(xx),scaley=False,alpha=0.3)
 
 plot()
-# draw some samples
-for i in range(4):
-	pb.plot(xx,myGP.sample(xx),scaley=False,alpha=0.3)
 myGP.find_kernel_params()
 plot()
-# draw some samples
-for i in range(4):
-	pb.plot(xx,myGP.sample(xx),scaley=False,alpha=0.3)
 pb.show()
 	